9975e6174746619004faa00ca1332a35f2a9bc58
mhyr_nvd <mahyarniavand65@gmail.com>
2025-12-13 23:01:41 +0330

    add stealth mode, add bat signal

--- Batman.cs
+++ Batman.cs
@@ -28,10 +28,15 @@ public class Batman : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : normalSpeed;
+        float currentSpeed = normalSpeed;
+
+        if (_currentState == BatmanState.Stealth)
+            currentSpeed = normalSpeed * 0.5f;
+
+        if (Input.GetKey(KeyCode.LeftShift) && _currentState != BatmanState.Stealth)
+            currentSpeed = runSpeed;
 
         Vector3 move = new Vector3(horizontal, vertical, 0);
-
         transform.Translate(move * currentSpeed * Time.deltaTime, Space.World);
     }
 
@@ -41,18 +46,27 @@ public class Batman : MonoBehaviour
         {
             _currentState = BatmanState.Normal;
             _uiManager.StopAlert();
+            _uiManager.ResetObejctOpacity();
         }
 
         if (Input.GetKeyDown(KeyCode.C))
         {
             _currentState = BatmanState.Stealth;
             _uiManager.StopAlert();
+            _uiManager.SetObjectOpacity(0.7f);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _currentState = BatmanState.Alert;
             _uiManager.StartAlert();
+            _uiManager.ResetObejctOpacity();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            _uiManager.ToggleBatSignal();
         }
     }
+
 }
--- UIManager.cs
+++ UIManager.cs
@@ -5,6 +5,19 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private GameObject RedAlarmPrefab;
 
+    [SerializeField]
+    private GameObject BatmanPrefab;
+
+    [SerializeField]
+    private GameObject GothamCity;
+
+    [SerializeField]
+    private GameObject BatSignalPrefab;
+
+    private SpriteRenderer _batmanSprite;
+    private SpriteRenderer _gothamCitySprite;
+    private SpriteRenderer _batSignalSprite;
+
     [SerializeField] 
     private AudioSource alertAudio;
 
@@ -13,9 +26,23 @@ public class UIManager : MonoBehaviour
     private bool _alertActive = false;
     private float _timer = 0f;
     private float _interval = 0.5f;
+    private float _rotationAmount = 5f;
+    private float _rotationSpeed = 1f;   
 
     void Start()
     {
+        _batmanSprite = BatmanPrefab.GetComponent<SpriteRenderer>();
+        if (_batmanSprite == null)
+            Debug.LogError("SpriteRenderer not found on BatmanPrefab");
+
+        _gothamCitySprite = GothamCity.GetComponent<SpriteRenderer>();
+        if (_gothamCitySprite == null)
+            Debug.LogError("SpriteRenderer not found on GothamCity");
+
+        _batSignalSprite = BatSignalPrefab.GetComponent<SpriteRenderer>();
+        if (_batSignalSprite == null)
+            Debug.LogError("SpriteRenderer not found on BatSignal");
+
         _alarmAudio = RedAlarmPrefab.GetComponent<AudioSource>();
         if (_alarmAudio == null)
             Debug.LogError("The Alarm Audio is NULL");
@@ -23,6 +50,15 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
+        if (BatSignalPrefab.activeSelf)
+        {
+            _timer += Time.deltaTime * _rotationSpeed;
+
+            float angle = Mathf.Sin(_timer) * _rotationAmount;
+
+            BatSignalPrefab.transform.localRotation = Quaternion.Euler(0f, 0f, angle);
+        }
+
         if (_alertActive)
         {
             _timer += Time.deltaTime;
@@ -52,5 +88,40 @@ public class UIManager : MonoBehaviour
 
         if (alertAudio.isPlaying)
             alertAudio.Stop();
+    }
+
+    public void SetObjectOpacity(float alpha)
+    {
+        if (_gothamCitySprite == null) return;
+        if (_batmanSprite == null) return;
+
+        Color gothamColor = _gothamCitySprite.color;
+        gothamColor.a = alpha;
+        _gothamCitySprite.color = gothamColor;
+
+        Color batmanColor = _batmanSprite.color;
+        batmanColor.a = alpha;
+        _batmanSprite.color = batmanColor;
+
+        Color batmanSignalColor = _batSignalSprite.color;
+        batmanSignalColor.a = alpha;
+        _batSignalSprite.color = batmanSignalColor;
+    }
+
+    public void ResetObejctOpacity()
+    {
+        SetObjectOpacity(1f);
+    }
+
+    public void ToggleBatSignal()
+    {
+        if (BatSignalPrefab.activeSelf)
+        {
+            BatSignalPrefab.SetActive(false);
+        }
+        else
+        {
+            BatSignalPrefab.SetActive(true);
+        }
     }
 }
